d971bb8dcdcd4945cd40b65a16b0fe63f92c467d
RAFAEL SCHUSTER <rsc1335@gmail.com>
2020-11-13 13:14:12 +0200

    Done Milestone 5

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const buton = document.getElementById('searchButton');
 const input = document.getElementById('inputField');
 const resultsList = document.querySelector('.list-group');
 const spinner = document.querySelector('.spin');
-const marquee = document.querySelector('.spanMarquee')
 let node;
 
 async function getFetch(url) {
@@ -16,38 +15,6 @@ async function fetchExtra(urlFull) {
     return data;
 }
 
-async function getMarquee(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data
-}
-
-async function marqueeText() {
-    await getMarquee('https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/quotes/nyse')
-        .then(info => {
-            let arrayQuotes = [];
-            for (let i = 0; i < info.length; i++) {
-                arrayQuotes[i] = Object.values(info[i]);
-                arrayQuotes[i] = arrayQuotes[i].splice(0, 3);
-            };
-            
-            arrayQuotes = arrayQuotes.flat()
-            for(let j = 1; j < arrayQuotes.length; j += 3){
-                arrayQuotes[j]= `:\u00A0 $`
-            }
-            arrayQuotes.unshift('Stock Prices in Real Time:\u00A0\u00A0\u00A0\u00A0')
-            arrayQuotes= arrayQuotes.join(' ')
-            marquee.innerText = arrayQuotes
-        })
-
-}
-
-marqueeText()
-
-function clearList() {
-    node.remove()
-}
-
 function addList(symbol, name, change, img) {
     spinner.className = 'spin spinner-border';
     setTimeout(() => {
@@ -55,25 +22,25 @@ function addList(symbol, name, change, img) {
         spinner.className = 'spin';
         let node = document.createElement('a');
         node.className = 'list-group-item';
-        let priceChanges = document.createElement('div')
-        priceChanges.className = 'priceChange'
-        change = change.replace(/[{()}]/g, '')
+        let priceChanges = document.createElement('div');
+        priceChanges.className = 'priceChange';
+        change = change.replace(/[{()}]/g, '');
         if (Math.sign(parseFloat(`${change}`)) == -1) {
-            priceChanges.className ='priceRed'}
-        let priceText = document.createTextNode(`${change}`)
-        priceChanges.appendChild(priceText)
+            priceChanges.className ='priceRed'};
+        let priceText = document.createTextNode(`${change}`);
+        priceChanges.appendChild(priceText);
         let textnode = document.createTextNode(`${name} (${symbol})\u00A0\u00A0\u00A0\u00A0 `);
         node.appendChild(textnode);
-        let image = document.createElement('img')
+        let image = document.createElement('img');
         image.setAttribute('src', img);
-        image.setAttribute('width', '40px')
-        image.setAttribute('height', '40px')
-        image.setAttribute('onerror', "this.src='Images/favicon_io/favicon-32x32.png'")
-        node.appendChild(image)
+        image.setAttribute('width', '40px');
+        image.setAttribute('height', '40px');
+        image.setAttribute('onerror', "this.src='Images/favicon_io/favicon-32x32.png'");
+        node.appendChild(image);
         node.title = `${name} (${symbol}) ${change}`;
         node.href = `company.html?symbol=${symbol}`;
         resultsList.appendChild(node);
-        resultsList.appendChild(priceChanges)
+        resultsList.appendChild(priceChanges);
     }, 1500);
 }
 
@@ -86,7 +53,7 @@ function getSearch(e) {
                 fetchExtra(`https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/${results[i].symbol}`)
                     .then(fullProfile => {
                         addList(fullProfile.symbol, fullProfile.profile.companyName, fullProfile.profile.changesPercentage, fullProfile.profile.image)
-                    })
+                    });
 
 
             };
---
+++ marquee.js
@@ -0,0 +1,31 @@
+export const marquee = document.querySelector('.spanMarquee');
+
+export async function getMarquee(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    const marqueeData = new marqueeConstruct(data, marquee);
+    marqueeData.report();
+    marqueeData.render();
+    return data;
+}
+
+export function marqueeConstruct(data, marqueePlaceholder) {
+    this.data = data;
+    this.report = () => {console.log(this.data)}
+    this.render = () =>{
+        let arrayQuotes = [];
+            for (let i = 0; i < this.data.length; i++) {
+                arrayQuotes[i] = Object.values(this.data[i]);
+                arrayQuotes[i] = arrayQuotes[i].splice(0, 3);
+            };
+            arrayQuotes = arrayQuotes.flat();
+            for(let j = 1; j < arrayQuotes.length; j += 3){
+                arrayQuotes[j]= `:\u00A0 $`;
+            }
+            arrayQuotes.unshift('Stock Prices in Real Time:\u00A0\u00A0\u00A0\u00A0');
+            arrayQuotes= arrayQuotes.join(' ');
+            marqueePlaceholder.innerText = arrayQuotes;
+            console.log(arrayQuotes);
+    }
+}
+
