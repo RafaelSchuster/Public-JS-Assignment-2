40ef3690bfbc096cf81cb9902af99e933888a3f6
RAFAEL SCHUSTER <rsc1335@gmail.com>
2020-11-17 23:32:27 +0200

    Done Milestone 8

--- marquee.js
+++ marquee.js
@@ -10,19 +10,18 @@ export async function getMarquee(url) {
 
 export function marqueeConstruct(data, marqueePlaceholder) {
     this.data = data;
-    this.render = () =>{
+    this.render = () => {
         let arrayQuotes = [];
-            for (let i = 0; i < this.data.length; i++) {
-                arrayQuotes[i] = Object.values(this.data[i]);
-                arrayQuotes[i] = arrayQuotes[i].splice(0, 3);
-            };
-            arrayQuotes = arrayQuotes.flat();
-            for(let j = 1; j < arrayQuotes.length; j += 3){
-                arrayQuotes[j]= `:\u00A0 $`;
-            }
-            arrayQuotes.unshift('Stock Prices in Real Time:\u00A0\u00A0\u00A0\u00A0');
-            arrayQuotes= arrayQuotes.join(' ');
-            marqueePlaceholder.innerText = arrayQuotes;
+        for (let i = 0; i < this.data.length; i++) {
+            arrayQuotes[i] = Object.values(this.data[i]);
+            arrayQuotes[i] = arrayQuotes[i].splice(0, 3);
+        };
+        arrayQuotes = arrayQuotes.flat();
+        for (let j = 1; j < arrayQuotes.length; j += 3) {
+            arrayQuotes[j] = `:\u00A0 $`;
+        }
+        arrayQuotes.unshift('Stock Prices in Real Time:\u00A0\u00A0\u00A0\u00A0');
+        arrayQuotes = arrayQuotes.join(' ');
+        marqueePlaceholder.innerText = arrayQuotes;
     }
 }
-
--- searchResults.js
+++ searchResults.js
@@ -1,22 +1,35 @@
 const spinner = document.querySelector('.spin');
 const resultsList = document.querySelector('.list-group');
+let i = 0;
+let companyArr = [];
 
 class SearchResult {
     constructor(results) {
         this.results = results;
     }
-    addList(symbol, name, change, img, search) {
+    compareCompany(companies, index) {
+        console.log(companies[index]);
+    };
+    addList(symbol, name, change, img, search, companies) {
         spinner.className = 'spin spinner-border';
+        companyArr[i] = companies;
+            i +=1;
         setTimeout(() => {
             spinner.className = 'spin';
             let listItem = document.createElement('a');
             listItem.className = 'list-group-item link';
+            listItem.style.position = 'relative';
             let priceChanges = document.createElement('div');
             priceChanges.className = 'priceChange';
+            const btnForm = document.createElement('form');
+            btnForm.style = 'display: inline';
+            btnForm.innerHTML = "<button class= 'compare' > compare </button>";
+            listItem.appendChild(btnForm);
             change = change.replace(/[{()}]/g, '');
             if (Math.sign(parseFloat(`${change}`)) == -1) {
                 priceChanges.className = 'priceRed';
             };
+            name = name.toUpperCase();
             let priceText = document.createTextNode(`${change}`);
             priceChanges.appendChild(priceText);
             let textnode = document.createTextNode(`${name} (${symbol})\u00A0\u00A0\u00A0\u00A0 `);
@@ -36,11 +49,20 @@ class SearchResult {
             search = search.toUpperCase();
             linkItem.forEach(each => {
                 each.innerHTML = each.innerHTML.replace(search, "<span style='background-color:   rgb(97, 192, 192)'>" + search + "</span>");
+                let butn = document.querySelectorAll('.compare');
+                butn = Array.from(butn);
+                for (let i = 0; i < butn.length; i++) {
+                    butn[i].addEventListener('click', (e) => {
+                        e.preventDefault();
+                        this.compareCompany(companyArr, i);
+                    });
+                };
+
             })
         }, 1500);
     }
     renderResults(companies, search) {
-        this.addList(companies.symbol, companies.profile.companyName, companies.profile.changesPercentage, companies.profile.image, search);
-    }
+        this.addList(companies.symbol, companies.profile.companyName, companies.profile.changesPercentage, companies.profile.image, search, companies);
+    };
 }
 const results = new SearchResult(document.getElementById('results'));
